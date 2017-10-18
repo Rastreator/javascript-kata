0500a0204c2a6c9d12a6a0333df8cddeb4fe5361
Alejandro <orta.sanz.93@gmail.com>
2017-10-18 21:16:09 +0200

    Workng with Test 2

---
+++ test_2/gulpfile.js
@@ -0,0 +1,13 @@
+// Comprimir imágenes
+// Las imágenes a comprimir están dentro de la carpeta src
+// se deben de enviar comprimidas a dist
+// en la carpeta dist se debe respetar la estructura de directorios de src
+// Tienes vía libre para utilizar la librería que más te guste
+
+var gulp = require('gulp')
+
+gulp.task('default', ['images'])
+
+gulp.task('images', function() {
+  // Empieza aquí
+})
